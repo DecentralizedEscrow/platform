ce2e1e69f4c711cf93fb3ec87c4a67f27091261a
PROrabbit <rabbit.mpt@gmail.com>
2018-07-11 13:50:59 +0300

    Восстановление пароля пользователя

---
+++ pass-restore.php
@@ -0,0 +1,41 @@
+<?php require_once('_top-guest.php'); ?>
+				<section class="passRestore col-md-12 row">
+					<div class="col-md-4 col-xs-12">
+						<div class="x_panel text-left">
+							<div class="x_title">
+							  <h2>Восстановить пароль</h2>
+							  <div class="clearfix"></div>
+							</div>
+							<div class="x_content">
+								<p><input class="form-control" type="password" name="pwd" placeholder="Введите новый пароль"></p>
+								<p><input class="form-control" type="password" name="pwd1" placeholder="Повторите пароль"></p>
+								<p><input class="form-control" type="text" name="pwd1" placeholder="Код подтверждения"></p>
+								<p><button class="btn btn-success btn-block">Задать пароль</button></p>
+							</div>
+						</div>
+					</div>
+				</section>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+
+      </div>
+      <!-- /page content -->
+    </div>
+
+  </div>
+  
+  <script src="js/bootstrap.min.js"></script>
+
+  <!-- bootstrap progress js -->
+  <script src="js/progressbar/bootstrap-progressbar.min.js"></script>
+  <script src="js/nicescroll/jquery.nicescroll.min.js"></script>
+
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
