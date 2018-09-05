c4ff64f4beff8ae850a53060f5778413a7f8a374
PROrabbit <rabbit.mpt@gmail.com>
2018-09-05 15:59:55 +0300

    devRequest-20180905
    по просьбе разработчиков бэка:
    - добавил стили для .innerSuccess и #ajSuccessModal .modal-content
    - перевязал стили conversModal с id на class

--- admin-user-moderate.php
+++ admin-user-moderate.php
@@ -609,7 +609,7 @@
 	</div>
 </div>
 <!-- окно просмотра беседы -->
-<div class="modal fade" id="conversModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="conversModal" class="conversModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
--- user - mail - list.php
+++ user - mail - list.php
@@ -149,7 +149,7 @@
 </div> 
 
 <!-- окно конкретной беседы -->
-<div class="modal fade" id="conversModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="conversModal" class="conversModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header">
