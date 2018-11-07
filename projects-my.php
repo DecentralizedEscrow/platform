56d876faa9cd49b0a292d7281e564695a122d9eb
PROrabbit <rabbit.mpt@gmail.com>
2018-11-07 18:24:13 +0300

    Добавлен блок завершенных проектов

--- projects-my.php
+++ projects-my.php
@@ -51,6 +51,57 @@
 							<div class="summ">Итого: $ 3 250</div>
 						</div>
 					</div>
+					<div class="x_panel">
+						<div class="x_title">
+						  <h2>Я участвовал</h2>
+						  <div class="clearfix"></div>
+						</div>
+						<div class="x_content">
+							<table class="table table-striped valign-m">
+								<thead>
+								  <tr>
+									<th><a href="#">Название <i class="fa fa-caret-down"></i></a></th>
+									<th><a href="#">Результат  <i class="fa fa-caret-down"></i></a></th>
+									<th><a href="#">Куплено токенов  <i class="fa fa-caret-down"></i></a></th>
+									<th></th>
+								  </tr>
+								</thead>
+								<tbody>
+								  <tr>
+									<td><a href="startup.php">Test project 1</a></td>
+									<td>
+										<div>
+											<div class="pLabel">
+												<div class="pull-left">Прогресс 175%</div>
+												<div class="pull-right">Завершено: 12.02.2019</div>
+												<div class="clearfix"></div>
+											</div>
+											<div class="progress"><div class="progress-bar progress-bar-info" data-transitiongoal="175" style="width: 100%;" aria-valuenow="175"></div></div>
+										</div>
+									</td>
+									<td>10 000 т = $ 1 450</td>
+									<td><a href="startup.php"><button type="button" class="btn btn-info btn-sm">Подробнее</button></a></td>
+								  </tr>
+								   <tr>
+									<td><a href="startup.php">Test project 6</a></td>
+									<td>
+										<div>
+											<div class="pLabel">
+												<div class="pull-left">Прогресс 22%</div>
+												<div class="pull-right">Недостаточное финансирование</div>
+												<div class="clearfix"></div>
+											</div>
+											<div class="progress"><div class="progress-bar progress-bar-danger" data-transitiongoal="22" style="width: 22%;" aria-valuenow="22"></div></div>
+										</div>
+									</td>
+									<td>12 500 т = $ 1 800</td>
+									<td><a href="startup.php"><button type="button" class="btn btn-info btn-sm">Подробнее</button></a></td>
+								  </tr>
+								</tbody>
+							</table>
+							<div class="summ">Итого: $ 3 250</div>
+						</div>
+					</div>
 				</section>
               </div>
             </div>
